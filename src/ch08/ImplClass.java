7c9c15121f449f9147c31638a9b7811d21590212
행복한호랑이PC\perl1 <perl1@naver.com>
2021-01-05 09:42:46 +0900

    인터페이스

---
+++ src/ch08/ImplClass.java
@@ -0,0 +1,23 @@
+package ch08;
+
+public class ImplClass implements Inter {
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ch08/InterImplMain.java
@@ -0,0 +1,14 @@
+package ch08;
+
+public class InterImplMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ImplClass im = new ImplClass();
+		im.method1();
+		im.method2();
+		
+
+	}
+
+}
