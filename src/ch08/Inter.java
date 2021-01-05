0e6a94b8823a125d861815850de51d5b26ae9dbe
행복한호랑이PC\perl1 <perl1@naver.com>
2021-01-05 09:33:50 +0900

    인터페이스 배우기

---
+++ src/ch08/Inter.java
@@ -0,0 +1,10 @@
+package ch08;
+
+public interface Inter {
+//	인터페이스의 멤버 : 상수, 정적메서드, 디폴트메서드(1.8에서 추가), 추상메서드
+	public void method1();
+	public void method2();
+	public void method3();
+//	ddddd
+
+}
